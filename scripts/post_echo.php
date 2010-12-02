846753e58f616edf527364d5ca9e0d7f0b9a4311
vecna <vecna@-e false.(none)>
2010-12-02 23:47:43 +0100

    simple adding in sniffjoke testsuite

---
+++ scripts/post_echo.php
@@ -0,0 +1,18 @@
+<?php
+
+/* this is the server script used in sniffjoke-testsuite */
+
+if(isset($_POST['sparedata'])) {
+        for($x = 0; $x < strlen($_POST['sparedata']); $x++)
+        {
+                if( is_numeric($_POST['sparedata'][$x]) )
+                        continue;
+
+                echo "bad value in $x offset";
+                exit;
+        }
+        echo $_POST['sparedata'];
+}
+
+?>
+
